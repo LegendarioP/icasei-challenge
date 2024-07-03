e18a2b4847c77a9d80fe0654dcc7c5ef78612a82
Pablo Luan <pablo-544@hotmail.com>
2024-07-03 15:26:03 -0300

    Update on some colors and fix functions, load, modal and favorite

--- web/scripts/script-favorite.js
+++ web/scripts/script-favorite.js
@@ -1,11 +1,14 @@
 var myFavorites
 var nextToken = '';
-HTMLData = function(idVid, thumbnail, title, isFav) {
+function HTMLData(idVid, thumbnail, title, isFav) {
     return(`
         <li class="card-video loading-configs">
             <div class="thumbnail-img" data-id="${idVid}">
                 <div class="svg-play">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="62" viewBox="0 -960 960 960" width="62" fill="#000000"><path d="m383-310 267-170-267-170v340Zm97 230q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                        <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
+                    </svg>
                 </div>
                 <img src="${thumbnail}" alt="${title}" title="${title}">
             </div>
@@ -19,15 +22,87 @@ HTMLData = function(idVid, thumbnail, title, isFav) {
 }
 
 
+function modalHTML(titleVideo, idVideo, isFav, token){
+    const htmlVideo = `
+        <div class="modal-video active">
+            <div class="modal-content" data-id="${idVideo}">
+                <div class="modal-header">
+                    <h2>${titleVideo}</h2>
+                    <div class="favorite ${isFav ? "active" : ""}">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40" fill="#d1d1d1">
+                            <path d="m233-120 65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"></path>
+                        </svg>
+                    </div>
+            </div>
+            <div class="modal-body">
+                <iframe src="https://www.youtube.com/embed/${idVideo}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="close-modal">FECHAR</button>
+            </div>
+            
+        </div>
+    </div>
+    `
+   document.body.insertAdjacentHTML("afterbegin", htmlVideo)
+
+    const closeButton = document.querySelector(".modal-video .close-modal")
+    const modalVideo = document.querySelector(".modal-video")
+    const favoriteModalVideo = document.querySelector(".modal-video .favorite")
+
+    function closeModal() {
+        modalVideo.remove();
+    }
+    closeButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        closeModal();
+    });
+    modalVideo.addEventListener("click", function(event) {
+        if (event.target === modalVideo) {
+            closeModal();
+        }
+    });
+
+    favoriteModalVideo.addEventListener("click", function(event) {
+        event.preventDefault()
+        if(token) {
+            const id_video = event.target.closest(".modal-content").dataset.id
+            const favoritedItem = event.currentTarget
+            !event.currentTarget.classList.contains("active") ? favoriteVideo(id_video, favoritedItem, token) : removeFavoritedVideo(id_video, favoritedItem, token)
+        }
+        else {
+            console.log("voce deve estar logado!")
+        }
+        
+    })
+}
+
 
 function updateFavoriteValues(value){
     const favoritesLength = document.querySelector("#favorites .favorite-number")
     return favoritesLength.innerHTML = value
 }
+
+
 function removeLoadingClass() {
     const loadingItens = document.querySelectorAll(".loading-configs")
     loadingItens.forEach(function(loadingItem){
         return loadingItem.classList.remove("loading-configs")
+    })
+}
+
+
+function openAndGenerateModal(token){
+    const openModalVideo = document.querySelectorAll(".loading-configs .thumbnail-img")
+    openModalVideo.forEach(function(modalPlay){
+        modalPlay.addEventListener("click", function(event) {
+            event.preventDefault()
+            const getIdVid = event.currentTarget.dataset.id
+            const getTitleVid = event.currentTarget.querySelector("img").getAttribute("title")
+            const isFavorited = event.currentTarget.closest(".card-video").querySelector(".favorite").classList.contains("active") ? true : false
+            modalHTML(getTitleVid, getIdVid, isFavorited, token)
+        })
+
     })
 }
 
@@ -43,7 +118,15 @@ async function favoriteVideo(idVid, favoritedItem, token) {
             // console.log(response.data)
             // console.log(favoritedItem)
             myFavorites.push(response.data)
-            favoritedItem.classList.add("active")
+            
+            if(document.querySelectorAll(`[data-id="${idVid}"]`).length > 1){
+                document.querySelector(`.card-video [data-id="${idVid}"]`).closest(".card-video").querySelector(".favorite").classList.add("active")
+                favoritedItem.classList.add("active")
+            }
+            else {
+                favoritedItem.classList.add("active")
+            }
+
             updateFavoriteValues(myFavorites.length)
         })
     } catch (error) {
@@ -61,7 +144,13 @@ async function removeFavoritedVideo(idVid, favoritedItem, token) {
             }
         })
         myFavorites = myFavorites.filter(item => item.youtube_idVid !== idVid);
-        favoritedItem.classList.remove("active")
+        if(document.querySelectorAll(`[data-id="${idVid}"]`).length > 1){
+            document.querySelector(`.card-video [data-id="${idVid}"]`).closest(".card-video").querySelector(".favorite").classList.remove("active")
+            favoritedItem.classList.remove("active")
+        }
+        else {
+            favoritedItem.classList.remove("active")
+        }
         updateFavoriteValues(myFavorites.length)
     } catch (error) {
         console.error(error)
@@ -69,22 +158,22 @@ async function removeFavoritedVideo(idVid, favoritedItem, token) {
 }
 
 function favoriteButton(token) {
-    const favoriteButton = document.querySelectorAll(".favorite")
+    const favoriteButton = document.querySelectorAll(".loading-configs .favorite")
     favoriteButton.forEach(function(button) {
         button.addEventListener("click", function(event) {
-            const id_video = event.target.closest(".card-video").querySelector(".thumbnail-img").dataset.id
-            const favoritedItem = event.currentTarget
-            if(!event.currentTarget.classList.contains("active")){
-                favoriteVideo(id_video, favoritedItem, token)
+            if(token){
+                const id_video = event.target.closest(".card-video").querySelector(".thumbnail-img").dataset.id
+                const favoritedItem = event.currentTarget
+                !event.currentTarget.classList.contains("active") ? favoriteVideo(id_video, favoritedItem, token) : removeFavoritedVideo(id_video, favoritedItem, token)    
             }
             else {
-                removeFavoritedVideo(id_video, favoritedItem, token)    
+                console.log("voce deve estar logado!")
             }
         });
     });
 }
 
-async function homePage (element, token){
+async function favoritePage (element, token){
 
     if(token){
         try {
@@ -124,8 +213,9 @@ async function homePage (element, token){
             }).join('')
 
             element.innerHTML = loadedVids;
-            removeLoadingClass()
             favoriteButton(token)
+            openAndGenerateModal(token)
+            removeLoadingClass()
          })
     } catch (error) {
         element.innerHTML = '<h2>OPS, ocorreu um erro ao buscar os videos favoritados, tente novamente em alguns minutos....</h2>'
@@ -134,73 +224,16 @@ async function homePage (element, token){
         element.classList.remove("loading")
     }
 }
-async function loadNextVideos(element, token){
-    // console.log(nextToken)
-    if(nextToken != ''){
-        try {
-            await axios.post('http://localhost:3333/loadvideos', {
-                pageToken: nextToken
-            })
-            .then(response => {
-                console.log(response.data)
-                const vids = response.data.items
-                const loadedVids = vids.map(item => {
-                    const getURL = item.id
-                    const hasFavorited = myFavorites.find(item => item.youtube_idVid == getURL) ? true : false
-                    const titleVid = item.snippet.title
-                    const thumbnail = item.snippet.thumbnails.maxres ? item.snippet.thumbnails.maxres.url : item.snippet.thumbnails.high.url
-                    return HTMLData(getURL, thumbnail, titleVid, hasFavorited)
-                }).join('')
-                element.insertAdjacentHTML("beforeend", loadedVids);
-                favoriteButton(token)
-                removeLoadingClass()
-                nextToken = response.data.nextPageToken ? response.data.nextPageToken : ''
-            })
-        } catch (error) {
-            element.innerHTML = '<h2>OPS, ocorreu um erro ao fazer a busca....</h2>'
-            console.error(error)
-        } finally {
-            element.classList.remove("loading")
-        }
-    }
-}
-
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log('O DOM está pronto!');
     const bodyVids = document.querySelector(".videos-body ul");
     let token = Cookies.get("token")
 
-    homePage(bodyVids, token)
-
-    
-
-
-
-    // favoriteButton.addEventListener("click", function(event) {
-    //     event.preventDefault()
-
-    //     console.log("oi")
-    // })
-
-
-    const elemento = document.querySelector('.videos-body');
-    let scrollActive = false
-    elemento.addEventListener('scroll', function() {
-        const scrollTop = elemento.scrollTop;
-        const elementHeight = elemento.scrollHeight;
-        const viewableHeight = elemento.clientHeight;
-
-        const scrollPercentage = (scrollTop + viewableHeight) / elementHeight;
-
-        if (scrollPercentage >= 0.9) {
-            if(!scrollActive){
-                scrollActive = true
-                loadNextVideos(bodyVids, token)
-            }
-        }
-        else {
-            scrollActive = false
-        }
-    });
+    if(token){
+        favoritePage(bodyVids, token)
+    }
+    else {
+        bodyVids.innerHTML = '<h2>Para acessar os seus videos favoritos será necessario realizar o login</h2>'
+    }
 });
--- web/scripts/script-home.js
+++ web/scripts/script-home.js
@@ -8,7 +8,10 @@ function HTMLData(idVid, thumbnail, title, isFav) {
         <li class="card-video loading-configs">
             <div class="thumbnail-img" data-id="${idVid}">
                 <div class="svg-play">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="62" viewBox="0 -960 960 960" width="62" fill="#000000"><path d="m383-310 267-170-267-170v340Zm97 230q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                        <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
+                    </svg>
                 </div>
                 <img src="${thumbnail}" alt="${title}" title="${title}">
             </div>
@@ -65,15 +68,15 @@ function modalHTML(titleVideo, idVideo, isFav, token){
 
     favoriteModalVideo.addEventListener("click", function(event) {
         event.preventDefault()
-        const id_video = event.target.closest(".modal-content").dataset.id
-        const favoritedItem = event.currentTarget
-        if(!event.currentTarget.classList.contains("active")){
-            // console.log("hi")
-            favoriteVideo(id_video, favoritedItem, token)
+        if(token) {
+            const id_video = event.target.closest(".modal-content").dataset.id
+            const favoritedItem = event.currentTarget
+            !event.currentTarget.classList.contains("active") ? favoriteVideo(id_video, favoritedItem, token) : removeFavoritedVideo(id_video, favoritedItem, token)
         }
         else {
-            removeFavoritedVideo(id_video, favoritedItem, token)    
+            console.log("voce deve estar logado!")
         }
+        
     })
     
     
@@ -163,20 +166,19 @@ function favoriteButton(token) {
     const favoriteButton = document.querySelectorAll(".loading-configs .favorite")
     favoriteButton.forEach(function(button) {
         button.addEventListener("click", function(event) {
-            const id_video = event.target.closest(".card-video").querySelector(".thumbnail-img").dataset.id
-            const favoritedItem = event.currentTarget
-            if(!event.currentTarget.classList.contains("active")){
-                favoriteVideo(id_video, favoritedItem, token)
+            if(token){
+                const id_video = event.target.closest(".card-video").querySelector(".thumbnail-img").dataset.id
+                const favoritedItem = event.currentTarget
+                !event.currentTarget.classList.contains("active") ? favoriteVideo(id_video, favoritedItem, token) : removeFavoritedVideo(id_video, favoritedItem, token)    
             }
             else {
-                removeFavoritedVideo(id_video, favoritedItem, token)    
+                console.log("voce deve estar logado!")
             }
         });
     });
 }
 
 async function homePage (element, token){
-
     if(token){
         try {
             await axios.get("http://localhost:3333/favorites",{
@@ -203,7 +205,7 @@ async function homePage (element, token){
             // console.log(response.data)
             const loadedVids = vids.map(item => {
                 const getURL = item.id
-                const hasFavorited = myFavorites.find(item => item.youtube_idVid == getURL) ? true : false
+                const hasFavorited = myFavorites ? myFavorites.find(item => item.youtube_idVid == getURL) ? true : false : false
                 // console.log(hasFavorited)
                 // console.log(getURL)
                 const titleVid = item.snippet.title
@@ -241,7 +243,7 @@ async function searchVids (bodyVids, params, token){
             const loadingVids = vids.map(item => {
                 const getURL = item.id.videoId
                 const titleVid = item.snippet.title
-                const hasFavorited = myFavorites.find(item => item.youtube_idVid == getURL) ? true : false
+                const hasFavorited = myFavorites ? myFavorites.find(item => item.youtube_idVid == getURL) ? true : false : false
                 const thumbnail = item.snippet.thumbnails.maxres ? item.snippet.thumbnails.maxres.url : item.snippet.thumbnails.high.url
                 return HTMLData(getURL,thumbnail, titleVid, hasFavorited)
             }).join('')
@@ -277,7 +279,7 @@ async function loadNextVideos(element, token){
                 const vids = response.data.items
                 const loadedVids = vids.map(item => {
                     const getURL = item.id
-                    const hasFavorited = myFavorites.find(item => item.youtube_idVid == getURL) ? true : false
+                    const hasFavorited = myFavorites ? myFavorites.find(item => item.youtube_idVid == getURL) ? true : false : false
                     const titleVid = item.snippet.title
                     const thumbnail = item.snippet.thumbnails.maxres ? item.snippet.thumbnails.maxres.url : item.snippet.thumbnails.high.url
                     return HTMLData(getURL, thumbnail, titleVid, hasFavorited)
@@ -297,7 +299,7 @@ async function loadNextVideos(element, token){
     }
 }
 async function loadNextSearchVideos(element, token){
-    console.log("entrou no search: ",nextToken)
+    console.log("entrou no search: ", nextToken)
     if(nextToken != ''){
         try {
             await axios.post('http://localhost:3333/search', {
@@ -308,8 +310,8 @@ async function loadNextSearchVideos(element, token){
                 console.log(response.data)
                 const vids = response.data.items
                 const loadedVids = vids.map(item => {
-                    const getURL = item.id
-                    const hasFavorited = myFavorites.find(item => item.youtube_idVid == getURL) ? true : false
+                    const getURL = item.id.videoId
+                    const hasFavorited = myFavorites ? myFavorites.find(item => item.youtube_idVid == getURL) ? true : false : false
                     const titleVid = item.snippet.title
                     const thumbnail = item.snippet.thumbnails.maxres ? item.snippet.thumbnails.maxres.url : item.snippet.thumbnails.high.url
                     return HTMLData(getURL, thumbnail, titleVid, hasFavorited)
@@ -340,8 +342,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     homePage(bodyVids, token)
 
-
-    
     buttonForm.addEventListener("click", function(event) {
         event.preventDefault();
         if(inputSearch.value == '') return 
